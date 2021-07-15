5c199d3f46193d578e4a42c97de7a16c0fd26cd4
momsse <1344228+momsse@users.noreply.github.com>
2021-07-15 12:36:20 +0200

    ex4: perspective decoration

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 import CharacterEditor from './components/CharacterEditor';
 import Footer from './components/Footer';
+import LightBar from './components/LightBar'
 
 function App() {
   return (
     <>
+      <LightBar />
       <CharacterEditor />
       <Footer />
     </>
---
+++ src/components/LightBar/LightBar.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import styles from './LightBar.module.css';
+
+const LightBar = () => (<div className={styles.bar} />);
+
+export default LightBar;
